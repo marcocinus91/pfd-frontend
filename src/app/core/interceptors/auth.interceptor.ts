c23aa74c4bfbc8125a6332f614f6cd59a15f713e
Marco <marco.cinus@abinsula.com>
2026-06-08 15:40:24 +0200

    feat: add auth interceptor with JWT and 401 handling

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpInterceptorFn, HttpErrorResponse } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError, throwError } from "rxjs";
+import { AuthService } from "../services/auth.service";
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    const token = authService.getToken();
+
+    const authReq = token
+        ? req.clone({ headers: req.headers.set('Autthorization', `Bearer ${token}`) })
+        : req;
+
+    return next(authReq).pipe(
+        catchError(error => {
+            if (error instanceof HttpErrorResponse && error.status == 401) {
+                authService.logout();
+                router.navigate(['/auth/login']);
+            }
+            return throwError(() => error );
+        })
+    )
+}
